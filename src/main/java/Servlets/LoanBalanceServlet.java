12ac1397495b52c2d46038c7d6b9f8328b2b24e7
cml5444 <52017499+cmlef5444@users.noreply.github.com>
2020-08-07 21:05:21 -0400

    Revert "Revert "Conflict Resolve""
    This reverts commit 96f1098f4bffbb3194086ac7c5cbde77eb4ca69f.

--- src/main/java/Servlets/LoanBalanceServlet.java
+++ src/main/java/Servlets/LoanBalanceServlet.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -89,3 +90,96 @@ public class LoanBalanceServlet extends HttpServlet {
     }// </editor-fold>
 
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import LoanBalance.BalanceCntl;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSetMetaData;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author cjani
+ */
+public class LoanBalanceServlet extends HttpServlet {
+
+    int customerId;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try{
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        customerId = (int)request.getSession().getAttribute("customerId");
+        System.out.println("LBS customerId: " + customerId);
+        BalanceCntl balance = new BalanceCntl();
+        balance.getSearchRecords();
+        
+       RequestDispatcher view = request.getRequestDispatcher("loanBalance.jsp");
+        view.forward(request, response);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+>>>>>>> 1426bbe715428f07dbe2899531e93e4a3dc8d476
