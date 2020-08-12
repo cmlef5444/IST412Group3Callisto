3c405a220707f8bc7823b7448774ef430890d2ea
cml5444 <52017499+cmlef5444@users.noreply.github.com>
2020-08-11 20:56:54 -0400

    Revert "Revert "Conflict Resolve""
    This reverts commit 1bca5dea3ca6e1c88d450bac039ef62aafe36c80.

--- src/main/java/Register/RegisterCntl.java
+++ src/main/java/Register/RegisterCntl.java
@@ -19,7 +19,7 @@ public class RegisterCntl {
      */
 
     protected ArrayList<Customer> customerArray;
-    
+
     protected String userEmail;
     protected String password;
     protected String key;
@@ -95,4 +95,40 @@ public class RegisterCntl {
         CustomerList customerList = new CustomerList();
         customerList.addCustomer(userEmail, password, "", "", "", "");
     }
+//    API methods for sending verification email and checking if the verification has been completed. Connection to database unresolved.
+//    public void sendVerification(userEmail) {
+//      private final String ACCOUNT_SID = "ACf75d5800803a66da30e66c954349e05c";
+//      private final String AUTH_TOKEN = "AUTH_TOKEN";
+//
+//      Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+//      Verification verification = Verification.creator(
+//              "VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+//              userEmail,
+//              "email")
+//          .setChannelConfiguration(
+//              new HashMap<String, Object>()
+//              {{
+//                  put("template_id", "d-efc5ca67718c4d589f3a37e9aa7a2349");
+//                  put("from", "verification@callisto.com");
+//                  put("from_name", "Callisto Finance");
+//              }})
+//          .create();
+//
+//      System.out.println(verification.getSid());
+//  }
+//
+//
+//
+//    public void EmailCheck(String userEmail) {
+//      private final String ACCOUNT_SID = "ACf75d5800803a66da30e66c954349e05c";
+//      private final String AUTH_TOKEN = "AUTH_TOKEN";
+//
+//      Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+//      VerificationCheck verificationCheck = VerificationCheck.creator(
+//              "VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+//              "123456")
+//          .setTo(userEmail).create();
+//
+//      System.out.println(verificationCheck.getSid());
+//  }
 }
